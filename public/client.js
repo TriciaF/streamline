4d1dd8baa66fc4f6a9f5fa53289b54ca91b8774c
TriciaF <TriciaF@thinkful.com>
2018-04-20 16:29:32 -0400

    added favicon removed console.log

--- public/client.js
+++ public/client.js
@@ -8,10 +8,8 @@ const boxOfficeURL = streamURL + '/favorites';
 
 ///////////HTML Templates////////////////
 function movieListTemplate(item) {
-  console.log('movieListTemplate');
-
-  return `<li class = "movie-item">
-    <p><span class="movie-title">${item.title}</span></p>
+	return `<li class = "movie-item">
+    <p class="movie-titles"><span class="movie-title">${item.title}</span></p>
     <div class = "id" style="display:none;">${item._id}</div>
     <div class = "poster-path"><img src="http://image.tmdb.org/t/p/w185/${item.poster_path}"</></div>
     <div class = "release-date">Release Date:  ${item.release_date}</div>
@@ -24,9 +22,7 @@ function movieListTemplate(item) {
 }
 
 function movieCardTemplate(item) {
-  console.log('movieCardTemplate');
-  console.log(item);
-  return `<li class = "movie-card">
+	return `<li class = "movie-card">
     <span class="movie-title-card">${item.title}</span>
     <div class = "id" style="display:none;">${item.id}</div>
     <div class = "poster-path"><img src="http://image.tmdb.org/t/p/w185//${item.poster_path}"</></div>
@@ -53,14 +49,12 @@ function movieCardTemplate(item) {
 }
 
 function boxOfficeTemplate(item) {
-  console.log('boxOfficeTemplate');
+	const rating = item.user_rating ? item.user_rating : '';
+	const comment = item.comment ? item.comment : '';
 
-  const rating = item.user_rating ? item.user_rating : '';
-  const comment = item.comment ? item.comment : '';
-
-  return `<li class = "box-office" >
+	return `<li class = "box-office" >
         <div class = "box-office-card" >
-        <p><span class = "movie-title-card" >${item.title} </span></p>
+        <pi class="movie-titles"><span class = "movie-title-card" >${item.title} </span></p>
         <div class = "id" style = "display:none;" >${item._id}</div> 
         <div class = "poster-path"> 
           <img src = "http://image.tmdb.org/t/p/w185//${item.poster_path}"></>
@@ -96,204 +90,189 @@ function boxOfficeTemplate(item) {
 
 /////////////////API Calls/////////////////////////
 function getMovieList() {
-  console.log('getMovieList');
-
-  $.ajax({
-    url: streamURL,
-    data: {
-      format: 'json' //GET Movie Collection
-    },
-    success: function(data) {
-      displayMovieList(data);
-    },
-    dataType: 'json',
-    contentType: 'application/json',
-    type: 'GET'
-  });
+	$.ajax({
+		url: streamURL,
+		data: {
+			format: 'json' //GET Movie Collection
+		},
+		success: function(data) {
+			displayMovieList(data);
+		},
+		dataType: 'json',
+		contentType: 'application/json',
+		type: 'GET'
+	});
 } //end getMovieList
 
 
 function addToBoxOffice() {
-  console.log('addToBoxOffice');
-
-  $('#movie-list-form').on('click', '.add-to-box-office', event => {
-    console.log('addToBoxOffice');
-    event.preventDefault();
-    let currentItem = event.currentTarget;
-    let id = $(currentItem).closest('li').find('div.id');
-    id = id[0].innerText;
-    $.ajax({ //Add movie to Favorites collection
-      dataType: 'json',
-      url: streamURL,
-      data: JSON.stringify({ id }),
-      success: function(data) {
-        changeButtonToAdded(data);
-      },
-      contentType: 'application/json',
-      type: 'POST'
-    });
-  });
+	$('#movie-list-form').on('click', '.add-to-box-office', event => {
+		event.preventDefault();
+		let currentItem = event.currentTarget;
+		let id = $(currentItem).closest('li').find('div.id');
+		id = id[0].innerText;
+		$.ajax({ //Add movie to Favorites collection
+			dataType: 'json',
+			url: streamURL,
+			data: JSON.stringify({ id }),
+			success: function(data) {
+				changeButtonToAdded(data);
+			},
+			contentType: 'application/json',
+			type: 'POST'
+		});
+	});
 } //end addToBoxOffice
 
 function getMovieCard() {
-  console.log('getMovieCard');
-  $('#movie-list-form').on('click', '.view-movie', event => {
-    event.preventDefault();
-    let id = $(event.currentTarget).closest('li').find('div.id');
-    let _id = id[0].innerHTML;
+	$('#movie-list-form').on('click', '.view-movie', event => {
+		event.preventDefault();
+		let id = $(event.currentTarget).closest('li').find('div.id');
+		let _id = id[0].innerHTML;
 
-    $.ajax({
-      url: streamURL + '/' + _id,
-      data: { //GET/:id from Movie Collection
-        format: 'json'
-      },
-      success: function(data) {
-        displayMovieCard(data);
-      },
-      type: 'GET'
-    });
-  });
+		$.ajax({
+			url: streamURL + '/' + _id,
+			data: { //GET/:id from Movie Collection
+				format: 'json'
+			},
+			success: function(data) {
+				displayMovieCard(data);
+			},
+			type: 'GET'
+		});
+	});
 } //end getMovieCard
 
 function getBoxOfficeList() {
-  console.log('getBoxOfficeList');
-
-  $.ajax({
-    url: boxOfficeURL,
-    success: function(data) {
-      displayBoxOffice(data); //GET from Favorites collection
-    },
-    dataType: 'json',
-    contentType: 'application/json',
-    type: 'GET'
-  });
+	$.ajax({
+		url: boxOfficeURL,
+		success: function(data) {
+			displayBoxOffice(data); //GET from Favorites collection
+		},
+		dataType: 'json',
+		contentType: 'application/json',
+		type: 'GET'
+	});
 } //end getMovieList
 
 
 
 function updateBoxOffice() {
-  console.log('updateBoxOffice');
-  $('#movie-list-form').on('click', '.update-movie', event => {
-    event.preventDefault();
-    let rating = $(event.currentTarget).closest('li').find('#user-rating').val();
+	$('#movie-list-form').on('click', '.update-movie', event => {
+		event.preventDefault();
+		let rating = $(event.currentTarget).closest('li').find('#user-rating').val();
 
-    let comment = $(event.currentTarget).closest('li').find('#user-comments').val();
+		let comment = $(event.currentTarget).closest('li').find('#user-comments').val();
 
-    let id = $(event.currentTarget).closest('li').find('div.id');
-    id = id[0].innerText;
+		let id = $(event.currentTarget).closest('li').find('div.id');
+		id = id[0].innerText;
 
 
-    $.ajax({ //Update movie from Favorites Collection
-      dataType: 'json',
-      url: streamURL + '/' + id,
-      data: JSON.stringify({ user_rating: rating, comment: comment }),
-      success: function(data) {
-        getBoxOfficeList(data);
-      },
-      contentType: 'application/json',
-      type: 'PUT'
-    });
-  });
+		$.ajax({ //Update movie from Favorites Collection
+			dataType: 'json',
+			url: streamURL + '/' + id,
+			data: JSON.stringify({ user_rating: rating, comment: comment }),
+			success: function(data) {
+				getBoxOfficeList(data);
+			},
+			contentType: 'application/json',
+			type: 'PUT'
+		});
+	});
 } //end updateBoxOffice
 
 
 function deleteFromBoxOffice() {
-  console.log('deleteFromBoxOffice');
-  $('#movie-list-form').on('click', '.remove-movie', event => {
-    event.preventDefault();
-    let id = $(event.currentTarget).closest('li').find('div.id');
-    id = id[0].innerText;
+	$('#movie-list-form').on('click', '.remove-movie', event => {
+		event.preventDefault();
+		let id = $(event.currentTarget).closest('li').find('div.id');
+		id = id[0].innerText;
 
-    $.ajax({ //Delete movie from Favorites Collection
-      dataType: 'json',
-      url: streamURL + '/' + id,
-      success: function(data) {
-        getBoxOfficeList(data);
-      },
-      contentType: 'application/json',
-      type: 'DELETE'
-    });
-  });
+		$.ajax({ //Delete movie from Favorites Collection
+			dataType: 'json',
+			url: streamURL + '/' + id,
+			success: function(data) {
+				getBoxOfficeList(data);
+			},
+			contentType: 'application/json',
+			type: 'DELETE'
+		});
+	});
 } //deleteFromBoxOffice
 
 function backToMovieList() {
-  $('#movie-list-form').on('click', '.back-to-list', event => {
-    $.ajax({
-      url: streamURL,
-      data: {
-        format: 'json' //GET movies from Movie Collection
-      },
-      success: function(data) {
-        displayMovieList(data);
-      },
-      type: 'GET'
-    });
-  });
+	$('#movie-list-form').on('click', '.back-to-list', event => {
+		$.ajax({
+			url: streamURL,
+			data: {
+				format: 'json' //GET movies from Movie Collection
+			},
+			success: function(data) {
+				displayMovieList(data);
+			},
+			type: 'GET'
+		});
+	});
 }
 
 
 //////////////////Render DOM Methods////////////////////
 function displayMovieList(data) {
-  console.log('displayMovieList');
-  $('.box-office-list').remove;
-  $('#movie-list-form').append('<ul class="movie-list"></ul>');
-  const movie = data.map((item) => movieListTemplate(item));
-  $('.movie-list').html(movie);
+	$('.box-office-list').remove;
+	$('#movie-list-form').append('<ul class="movie-list"></ul>');
+	const movie = data.map((item) => movieListTemplate(item));
+	$('.movie-list').html(movie);
 
 } //end displayMovieList
 
 
 function displayMovieCard(data) {
-  console.log('displayMovieCard');
-  $('.movie-list').remove();
-  $('#movie-list-form').append('<ul class="movie-card-list"></ul>');
-  const movie = movieCardTemplate(data);
-  $('.movie-card-list').html(movie);
+	$('.movie-list').remove();
+	$('#movie-list-form').append('<ul class="movie-card-list"></ul>');
+	const movie = movieCardTemplate(data);
+	$('.movie-card-list').html(movie);
 
 } //end displayMovieCard
 
 
 function changeButtonToAdded() {
-  $('.add-to-box-office').remove();
-  $('.movie-item-controls').prepend(`<button class = "added" disabled= "">
+	$('.add-to-box-office').remove();
+	$('.movie-item-controls').prepend(`<button class = "added" disabled= "">
 <span class = "button" >Added</span>`);
 }
 
 
 function goToBoxOffice() {
-  console.log('goToBoxOffice');
-
-  $('#movie-list-form').on('click', '.go-to-box-office-list', event => {
-    event.preventDefault();
-    getBoxOfficeList();
-  });
+	$('#movie-list-form').on('click', '.go-to-box-office-list', event => {
+		event.preventDefault();
+		getBoxOfficeList();
+	});
 }
 
 
 function displayBoxOffice(data) {
-  console.log('displayBoxOffice');
-  if (data.length !== 0) {
-    $('.movie-list').remove();
-    $('.movie-card-list').remove();
-    $('.box-office-list').remove();
-    $('#movie-list-form').append('<ul class="box-office-list"></ul>');
+	if (data.length !== 0) {
+		$('.movie-list').remove();
+		$('.movie-card-list').remove();
+		$('.box-office-list').remove();
+		$('#movie-list-form').append('<ul class="box-office-list"></ul>');
 
-    const movie = data.map((item) => boxOfficeTemplate(item));
-    $('.box-office-list').html(movie);
-  } else {
-    $('.movie-list').remove();
-    $('.movie-card-list').remove();
-    $('.box-office-list').remove();
-    getMovieList();
-  }
+		const movie = data.map((item) => boxOfficeTemplate(item));
+		$('.box-office-list').html(movie);
+	} else {
+		$('.movie-list').remove();
+		$('.movie-card-list').remove();
+		$('.box-office-list').remove();
+		getMovieList();
+	}
 } //end displayBoxOffice
 
 $(function() {
-  getMovieList();
-  updateBoxOffice();
-  addToBoxOffice();
-  deleteFromBoxOffice();
-  getMovieCard();
-  goToBoxOffice();
-  backToMovieList();
+	getMovieList();
+	updateBoxOffice();
+	addToBoxOffice();
+	deleteFromBoxOffice();
+	getMovieCard();
+	goToBoxOffice();
+	backToMovieList();
 });
